f6d5d2f0e70d347214961eaeb85136854d547f97
Leonard <wzdlc1996@gmail.com>
2018-12-08 18:52:19 +0800

    update Sat Dec  8 18:52:19 CST 2018

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
+cd public/
+rm $(ls|grep -v .git)
+cd ../
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 git add .
