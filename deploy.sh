680b139c38afdc133e15be44f60fdd9365591555
Leonard <wzdlc1996@gmail.com>
2018-12-12 15:41:14 +0800

    update Wed Dec 12 15:41:14 CST 2018

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,15 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
 cd public/
+
 rm -rf $(ls|grep -v .git)
+
 cd ../
+
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+cd public/
 # Add changes to git.
 git add .
 
@@ -20,17 +23,21 @@ git commit -m "$msg"
 # Push source and build repos.
 git push origin master
 
-# Come Back up to the Project Root
 cd ../themes/AllinOne/
+
 git add .
+
 msg="modifed at `date`"
+
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
 git push origin master
 
+# Come Back up to the Project Root
 cd ../../
 
 msg="update `date`"
